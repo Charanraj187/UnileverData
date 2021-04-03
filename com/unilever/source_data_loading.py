3ea9e45ca4e17b47d3129d0fa5f5889ebdda936e
kunalreddy <kunalreddyaleti@gmail.com>
2021-04-03 11:04:03 -0500

    start

--- com/unilever/source_data_loading.py
+++ com/unilever/source_data_loading.py
@@ -10,14 +10,6 @@ if __name__ == '__main__':
         '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
     )
 
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read ingestion enterprise applications") \
-        .master('local[*]') \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
@@ -26,6 +18,18 @@ if __name__ == '__main__':
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .config("spark.mongodb.output.uri", app_secret["mongodb_config"]["uri"])\
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+
 
     src_list = app_conf["source_data_list"]
 
