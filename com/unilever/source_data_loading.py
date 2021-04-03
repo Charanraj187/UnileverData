536ce29e2df0af161d5d68ad144286f077f390c5
kunalreddy <kunalreddyaleti@gmail.com>
2021-04-03 11:33:49 -0500

    start

--- com/unilever/source_data_loading.py
+++ com/unilever/source_data_loading.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
             #customer.withColumn("street", customer("address.street"))\
                     #.withColumn("city", customer("address.city"))\
                     #.withColumn("state",customer("address.state"))
-            customer.select(functions.col('address.street').alias('Street')).show()
+            customer.select(functions.col('customer_id'),functions.col('address.city').alias('city'),functions.col('address.state').alias('State')).show()
 
 
 
