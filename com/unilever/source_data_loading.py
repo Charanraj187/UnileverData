43e70d35990b6f9ca45fc334b55d9f05034a15b8
kunalreddy <kunalreddyaleti@gmail.com>
2021-04-03 10:18:50 -0500

    start

--- com/unilever/source_data_loading.py
+++ com/unilever/source_data_loading.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
         elif src == "CP":
             print("Reading CP data from S3 bucket name {}".format(src_conf["s3_conf"]["s3_bucket"]))
             txn_df3 = spark.read\
-                .option("fileType", "csv")\
+                .format("csv")\
                 .option("delimiter", "|")\
                 .load("s3a://" + src_conf["s3_conf"]["s3_bucket"] +"/" +src_conf["s3_conf"]["filename"])\
                 .withColumn("ins_dt" , functions.current_date())
